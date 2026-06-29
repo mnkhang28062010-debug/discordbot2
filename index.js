79bcde48e247e750c82bdf8729763fd8ce873a40
mnkhang28062010-debug <mnkhang28062010@gmail.com>
2026-06-29 23:02:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+const express = require("express");
 const { Client, GatewayIntentBits } = require("discord.js");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Web server started");
+});
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
 });
 
-client.once("ready", () => {
+client.once("clientReady", () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
