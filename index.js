0fbd1a24b2f4602bbb7a334cc82e16e2a97ea2e8
mnkhang28062010-debug <mnkhang28062010@gmail.com>
2026-06-29 23:16:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { Client, GatewayIntentBits } = require("discord.js");
+const { joinVoiceChannel } = require("@discordjs/voice");
 
 const app = express();
 
@@ -7,16 +8,28 @@ app.get("/", (req, res) => {
   res.send("Bot is running!");
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Web server started");
-});
+app.listen(process.env.PORT || 3000);
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates,
+  ],
 });
 
-client.once("clientReady", () => {
+client.once("clientReady", async () => {
   console.log(`Logged in as ${client.user.tag}`);
+
+  const guild = await client.guilds.fetch(process.env.GUILD_ID);
+
+  joinVoiceChannel({
+    channelId: process.env.VOICE_ID,
+    guildId: guild.id,
+    adapterCreator: guild.voiceAdapterCreator,
+    selfDeaf: false,
+  });
+
+  console.log("Joined voice channel!");
 });
 
 client.login(process.env.TOKEN);
