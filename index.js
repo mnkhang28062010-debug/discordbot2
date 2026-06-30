ba806ba7abdf26e5830d037c84fd909bb8d56beb
mnkhang28062010-debug <mnkhang28062010@gmail.com>
2026-06-30 18:57:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
-require("dotenv").config();
-
-const { Client, GatewayIntentBits, ChannelType } = require("discord.js");
+const express = require("express");
+const { Client, GatewayIntentBits } = require("discord.js");
 const { joinVoiceChannel } = require("@discordjs/voice");
 
-const client = new Client({
-    intents: [GatewayIntentBits.Guilds]
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
 });
 
-client.once("ready", async () => {
-    console.log(`Đăng nhập thành công: ${client.user.tag}`);
+app.listen(process.env.PORT || 3000);
 
-    try {
-        const guild = await client.guilds.fetch("1461352606021452022");
-        const channel = await guild.channels.fetch("1521192322899902637");
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates,
+  ],
+});
 
-        if (!channel || channel.type !== ChannelType.GuildVoice) {
-            return console.log("Không tìm thấy kênh voice.");
-        }
+client.once("clientReady", async () => {
+  console.log(`Logged in as ${client.user.tag}`);
 
-        joinVoiceChannel({
-            channelId: channel.id,
-            guildId: guild.id,
-           
+  const guild = await client.guilds.fetch(process.env.GUILD_ID);
+
+  joinVoiceChannel({
+    channelId: process.env.VOICE_ID,
+    guildId: guild.id,
+    adapterCreator: guild.voiceAdapterCreator,
+    selfDeaf: false,
+  });
+
+  console.log("Joined voice channel!");
+});
+
+client.login(process.env.TOKEN);
